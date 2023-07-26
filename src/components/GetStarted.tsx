b5589d55ed5f0b81b84f767a84b1b3d9e8cb00a4
iamsabet <iamsabet@gmail.com>
2023-07-26 11:34:55 +0330

    + Hero Section

--- src/components/GetStarted.tsx
+++ src/components/GetStarted.tsx
@@ -1,7 +1,39 @@
 import React from "react";
+import styles from "../style";
+import { arrowUp } from "../assets";
 
 const GetStarted = () => {
-  return <div>GetStarted</div>;
+  return (
+    <div
+      className={`${styles.flexCenter} w-[140px] h-[140px]
+    rounded-full bg-blue-gradient p-[2px] cursor-pointer`}
+    >
+      <div
+        className={`${styles.flexCenter} bg-primary w-full h-full 
+        rounded-full flex-col`}
+      >
+        <div className={`${styles.flexStart} flex-row`}>
+          <p
+            className="font-poppins font-medium text-[20px]
+            leading-[20px] text-gradient mr-4"
+          >
+            <span>Get</span>
+          </p>
+          <img
+            src={arrowUp}
+            className="w-[23px] h-[23px] object-contain"
+            alt="arrow"
+          />
+        </div>
+        <p
+          className="font-poppins font-medium text-[20px]
+            leading-[20px] text-gradient"
+        >
+          <span>Started</span>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default GetStarted;
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,83 @@
 import React from "react";
+import styles from "../style";
+import { discount, robot } from "../assets";
+import GetStarted from "./GetStarted";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section
+      id="home"
+      className={`flex md:flex-row flex-col ${styles.paddingY} justify-between items-start`}
+    >
+      <div
+        className={`flex flex-1 flex-col ${styles.flexStart} xl:px-0 ss:px-7 px-16`}
+      >
+        <div
+          className="flex flex-row items-center
+          py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2"
+        >
+          <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
+          <p className={`${styles.paragraph} ml-2`}>
+            <span className="text-white">20%</span> Discount For
+            <span className="text-white"> 1 Month</span> Account
+          </p>
+        </div>
+
+        <div className="flex flex-row justify-between items-center w-full">
+          <h1
+            className="flex-1 font-poppins font-semibold 
+            ss:text-[72px] text-[52px] text-white 
+            ss:leading-[100px] leading-[75px]"
+          >
+            The Next <br className="sm:block hidden" />
+            <span className="text-gradient">Generation</span>
+          </h1>
+          <div className="ss:flex hidden md:mr-4 mb-8">
+            <GetStarted />
+          </div>
+        </div>
+        <h1
+          className="font-poppins font-semibold 
+            ss:text-[62px] text-[48px] text-white 
+            ss:leading-[100px] leading-[75px] w-full"
+        >
+          Payment Method.
+        </h1>
+        <p className={`${styles.paragraph} max-w-[470px]`}>
+          Our team of experts uses a methodology to identify the credit cards
+          most likely to fit your needs. We examine annual percentage rates,
+          annual fees.
+        </p>
+      </div>
+      <div
+        className={`w-full flex flex-1 ${styles.flexCenter}
+          relative my-10`}
+      >
+        <img
+          src={robot}
+          alt="robot"
+          className="w-full max-w-[650px] min-w-[400px]
+          relative z-[5] ml-auto lg:mt-0 mt-10"
+        />
+        <div
+          className="absolute z-0 w-[40%] h-[40%]
+          top-0 right-0 pink__gradient"
+        />
+        <div
+          className="absolute z-1 rounded-20 w-[80%] h-[80%]
+          bottom-40 right-0 white__gradient"
+        />
+        <div
+          className="absolute z-0 w-[50%] h-[50%]
+          right-10 bottom-20 blue__gradient"
+        />
+      </div>
+
+      <div className={`ss:hidden ${styles.flexCenter} w-full`}>
+        <GetStarted />
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
